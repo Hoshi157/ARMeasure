21416eed393bf4185ffba9359bfa2c2b88e2de52
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-08-04 13:06:49 +0900

    first commit

---
+++ ARMeasure/View/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ARMeasure
+//
+//  Created by 福山帆士 on 2020/08/04.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+}
